6189651363aa5d5d3c2dec82f9908bf9b360b755
Abdullah Öztürk <abdullahozturk99@gmail.com>
2020-08-13 12:31:17 +0300

    -- NoteDto'daki UserId parametresinin türü degistirildi.

--- src/DevNots.Application/Contracts/NoteDto.cs
+++ src/DevNots.Application/Contracts/NoteDto.cs
@@ -8,7 +8,7 @@ namespace DevNots.Application.Contracts
     public class NoteDto : INoteDetails
     {
         public int Id { get; internal set; }
-        public int UserId { get; set; }
+        public string UserId { get; set; }
         public string Text { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
