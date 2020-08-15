d2fa59c4053971f242a8b57f540003b0690a8ab4
Abdullah Öztürk <abdullahozturk99@gmail.com>
2020-08-15 13:57:57 +0300

    -- Some Refactorings

--- src/DevNots.Application/Validations/NoteValidator.cs
+++ src/DevNots.Application/Validations/NoteValidator.cs
@@ -12,7 +12,20 @@ namespace DevNots.Application.Validations
     {
         public NoteValidator()
         {
-            RuleFor(rule => rule.Text).MinimumLength(1);
+            RuleFor(rule => rule.Text)
+                .NotEmpty().WithMessage(CustomMessage)
+                .MaximumLength(300);
+            RuleFor(rule => rule.Description)
+                .NotEmpty().WithMessage(CustomMessage)
+                .MaximumLength(50);
+            RuleFor(rule => rule.Title)
+                .NotEmpty().WithMessage(CustomMessage)
+                .MaximumLength(50);
+            RuleFor(rule => rule.CreatedAt)
+                .NotEmpty().WithMessage(CustomMessage)
+                .GreaterThan(DateTime.Now);
         }
+
+        private string CustomMessage = "{PropertyName} can not be empty";
     }
 }
